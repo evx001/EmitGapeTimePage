8d24e161dd47decafda38b523a237aab5a558032
evx001 <eviexeris@gmail.com>
2015-08-18 10:45:11 -0400

    commit pages

---
+++ EmitGape/EmitGape.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace EmitGape
+{
+
+	public class App : Application
+	{
+
+
+		public App ()
+		{
+
+			// The root page of your application
+
+
+			MainPage = new NavigationPage (new ContentPage {
+				Content = new Button {
+					Text = "What time is it?",
+					HorizontalOptions = LayoutOptions.CenterAndExpand,
+					VerticalOptions = LayoutOptions.CenterAndExpand,
+					Command = new Command (() => MainPage.Navigation.PushAsync (new TimePage ())),
+				},
+			});
+		}
+
+
+		//		public TimePage ()
+		//		{
+		//			ContentPage.Content = timeLabel;
+		//
+		//		}
+
+		readonly Label timeLabel = new Label {
+			Text = "Loading...",
+			HorizontalOptions = LayoutOptions.CenterAndExpand,
+			VerticalOptions = LayoutOptions.CenterAndExpand,
+
+		};
+
+
+		
+
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+
+
+
+	}
+
+
+
+}
+
---
+++ EmitGape/TimePage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace EmitGape
+{
+	public class  TimePage
+	{
+		public TimePage()
+		//					ContentPage.Content = timeLabel;
+
+
+	
+	//	protected override async void OnAppearing()
+	//	{
+	//		base.OnAppearing();
+	//		timeLabel.Text = await RequestTimeAsync();
+	//	}
+
+	//	static async Task<string> RequestTimeAsync()
+	//	{
+	//		using (var client = new HttpClient()) {
+	//			var jsonString = await client.GetStringAsync("http://date.jsontest.com/");
+	//			var jsonObject = JObject.Parse(jsonString);
+	//			return jsonObject["time"].Value<string>();
+	//		}
+	//	}
+
+}
+};
