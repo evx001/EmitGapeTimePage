aa4605a40d7d33156892326e1eb72f9ba48af53c
evx001 <eviexeris@gmail.com>
2015-08-20 00:39:55 -0400

    webView attempt

--- EmitGape/TimePage.cs
+++ EmitGape/TimePage.cs
@@ -3,45 +3,53 @@ using Xamarin.Forms;
 using System.Threading.Tasks;
 using System.Net.Http;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
 
 namespace EmitGape
 {
-    public class TimePage: ContentPage
-    {
-        //< timeLabel
-        readonly Label timeLabel = new Label {
-            Text = "Loading...",
-            HorizontalOptions = LayoutOptions.CenterAndExpand,
-            VerticalOptions = LayoutOptions.CenterAndExpand,
-        };
-        //> timeLabel
+	public class TimePage: ContentPage
+	{
+		//< timeLabel
+		readonly Label timeLabel = new Label {
+			Text = "Loading...",
+			HorizontalOptions = LayoutOptions.CenterAndExpand,
+			VerticalOptions = LayoutOptions.CenterAndExpand,
+		};
+		//> timeLabel
 
-        //< TimePage
-        public TimePage()
-        {
-            Content = timeLabel;
-        }
-        //> TimePage
+		//< TimePage
+		public TimePage ()
+		{
+			Content = timeLabel;
+		}
+		//> TimePage
 
-        //< OnAppearing
-        protected override async void OnAppearing()
-        {
-            base.OnAppearing();
-            timeLabel.Text = await RequestTimeAsync();
-        }
-        //> OnAppearing
+		//< OnAppearing
+		protected override async void OnAppearing ()
+		{
+			base.OnAppearing ();
+			timeLabel.Text = await RequestTimeAsync ();
+		}
+		//> OnAppearing
 
-        //< RequestTimeAsync
-        static async Task<string> RequestTimeAsync()
-        {
-            using (var client = new HttpClient()) {
-                var jsonString = await client.GetStringAsync("http://date.jsontest.com/");
-                var jsonObject = JObject.Parse(jsonString);
-                return jsonObject["time"].Value<string>();
-            }
-        }
-        //> RequestTimeAsync
-    }
+		//< RequestTimeAsync
+		static async Task<string> RequestTimeAsync ()
+		{
+			using (var client = new HttpClient ()) {
+				// http://openweathermap.org/
+
+				var jsonString = await client.GetStringAsync ("http://openweathermap.org/");
+				var jsonObject = JObject.Parse (jsonString);
+				return (string)jsonObject; // ["time"].Value<string> ();
+				var webView = new WebView ();
+				var htmlWebViewSource = new HtmlWebViewSource ();
+				htmlWebViewSource.Html = (string)jsonObject;
+				webView.Source = htmlWebViewSource;
+				WebView webview = webView;
+			}
+		}
+		//> RequestTimeAsync
+	}
 }
 
 
